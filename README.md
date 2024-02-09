e12911b1845c62466aec37b248849fdd83de5806
SamTredgett <sam.tredgett@gmail.com>
2024-02-09 22:44:57 +0000

    test

---
+++ README.md
@@ -0,0 +1,11 @@
+# Go Compiler
+
+Extending the previous implementation of the Go Interpreter into a fully fledged compiler
+which utilises a virtual machine.
+
+## In progress:
+    - Virtual Machine
+
+Note to the reader:
+Please feel free to fork, submit pull requests, and download and use code as you see fit.
+
---
+++ token/token.go
@@ -0,0 +1,73 @@
+package token
+
+// TokenType is the base format for the individual tokens we seek to identify in code parsed. These include operators, keywords and user-defined tokens
+type TokenType string
+
+// Token struct represents the token itself, what the string of it is and the in-code representation as a TokenType
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + literals
+	IDENT  = "IDENT" // add, foobar, x, y, ...
+	INT    = "INT"   // 1343467
+	STRING = "STRING"
+
+	// Operators
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
+
+	// Delimiters
+	COMMA     = ","
+	COLON     = ":"
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+	LBRACKET  = "["
+	RBRACKET  = "]"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
+
+	EQ     = "=="
+	NOT_EQ = "!="
+)
+
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+}
+
+// LookupIdent checks the keywords table t see whether the given identifier is in fact a keyword. If it is, return the keyword's TokenType
+// constant. Else, we just get back he token.IDENT, which is the TokenType for all user-defined identifiers.
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
